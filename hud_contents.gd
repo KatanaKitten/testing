7dd5c2f4d12044cbc88f459814b974a4d32f9804
KatanaKitten <133791672+KatanaKitten@users.noreply.github.com>
2025-05-20 12:13:17 -0400

    a

--- hud_contents.gd
+++ hud_contents.gd
@@ -1,8 +1,15 @@
 extends Label
 
 @onready var label: Label = $"."
+@onready var spd
 func _ready():
-	$"....".getSpeed.connect()
+	pass
 
 func _process(_delta):
-	label.text = "Speed: " + str() + " m/s"
+	label.text = "Speed: " + str(spd) + " m/s"
+
+
+
+
+func _on_player_get_speed(speed: Variant) -> void:
+	spd = speed
--- player.gd
+++ player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody3D
 
 signal getSpeed(speed)
-@export var speed = 14
+var speed
+const WALK_SPEED = 14
+const SPRINT_SPEED = 28
 @export var gravity = 9.8
 @export var sensitivity = 0.003
 
-var target_velocity = Vector3.ZERO
+#var target_velocity = Vector3.ZERO
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -22,30 +24,37 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = ($CameraPivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	if direction:
-		target_velocity.x = direction.x * speed
-		target_velocity.z = direction.z * speed
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = 0.0
+			velocity.z = 0.0
 	else:
-		target_velocity.x = 0.0
-		target_velocity.z = 0.0
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
-		target_velocity.y += 5
+		velocity.y += 5
 		
-	if Input.is_action_just_pressed("sprint"):
-		speed *= 2
-	if Input.is_action_just_released("sprint"):
-		speed /= 2
-	
+	#if Input.is_action_just_pressed("sprint"):
+		#speed = SPRINT_SPEED
+	#if Input.is_action_just_released("sprint"):
+		#speed = WALK_SPEED
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
 	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
 
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (gravity * delta)
+		velocity.y = velocity.y - (gravity * delta)
 
 	# Moving the Character
-	velocity = target_velocity
+	#velocity = target_velocity
 	getSpeed.emit(speed)
 	move_and_slide()
